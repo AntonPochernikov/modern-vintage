04af174c4270de13b2988c3a87fab95554a9a95d
antonpochernikov <akpochernikov@gmail.com>
2024-03-10 17:59:50 +0300

    feat: App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { App } from './App.tsx'
+import { App } from '@/components/App'
+
 import './global.module.css'
 
 const rootElement = document.getElementById('root')
